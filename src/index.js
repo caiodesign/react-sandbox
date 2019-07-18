de35714c498eed9c1649068496797c70177c8b06
Caio Oliveira <colive.dev@gmail.com>
2019-07-18 02:04:59 -0300

    now using react-hot-reload for a better environment'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-import React, {useState, useEffect} from 'react'
-import ReactDOM from "react-dom";
+import { hot } from 'react-hot-loader/root';
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 
 import Main from './containers/Main/index.jsx'
 
-const wrapper = document.getElementById("root");
-if (wrapper) ReactDOM.render(<Main />, wrapper);
+
+function AppComponent () {
+  return (
+    <AppContainer>
+      <Main />
+    </AppContainer>
+  )
+}
+
+if (module.hot) hot(module, AppComponent)
+render(<AppComponent />, document.getElementById('root'))
