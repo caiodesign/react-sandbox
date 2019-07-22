0b93e3f7f0e58b81af90b85fc1290d7741643192
Caio Oliveira <caioliveira@uolinc.com>
2019-07-22 01:27:42 -0300

    added a saga mock file just for demo purpose

---
+++ src/sagas/demo.js
@@ -0,0 +1,23 @@
+import { put, takeEvery, all } from 'redux-saga/effects'
+
+const delay = (ms) => new Promise(res => setTimeout(res, ms))
+
+function* helloSaga() {
+  console.log('Hello Sagas!')
+}
+
+function* incrementAsync() {
+  yield delay(1000)
+  yield put({ type: 'INCREMENT' })
+}
+
+function* watchIncrementAsync() {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+export default function* rootSaga() {
+  yield all([
+    helloSaga(),
+    watchIncrementAsync()
+  ])
+}
